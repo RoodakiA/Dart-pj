8c811fb18f7f92ad55b093944d0639255eabc8b3
Amir hossein roodaki <amirhosseinroudaki@yahoo.com>
2022-08-15 11:43:42 +0330

    second commit

--- main.dart
+++ main.dart
@@ -1,5 +1,24 @@
 
-main(){
- print("Hello,world!");
-
+main() {
+   var pride = car(model:"111", speed:"110", color: "white" );
+   var peikan = car(model:"javanan", speed:"100", color: "gojeii");
+   print(pride.model);
+   pride.move();
+   print(peikan.model);
+   print(peikan.color);
+   peikan.move();
 }
+ class car {
+ var color;
+ var speed;
+ var model;
+
+ move(){
+
+ }
+  stop(){
+    //stoping function
+  }
+   car({this.color, this.speed, this.model});
+
+ }
